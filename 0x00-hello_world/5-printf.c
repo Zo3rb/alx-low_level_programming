7fe2d4365b4ac8cfcb5020af6ab06b82e9729822
Zo3rb <forsteve79@gmail.com>
2022-09-08 09:24:36 -0700

    get used to betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
-* main - Entry point
-*
-* Description: a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
-*
-* Return: Always 0 (Success)
-*
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
