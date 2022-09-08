7cae319aeac10cba925cde0fecb4e91b1f259263
Zo3rb <forsteve79@gmail.com>
2022-09-08 10:05:36 -0700

    Still Hard

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: prints the standrad error sandwiched around the  standard output.
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: returns 1
- */
+ * Return: This time we return an error 1.
+*/
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
-
