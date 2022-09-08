748da94c3bc55d836bc2b2757198984cd3d17ad6
Zo3rb <forsteve79@gmail.com>
2022-09-08 09:55:27 -0700

    write again again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
  * main - Entry point
- * Return:1
+ *
+ * Description: prints the standrad error sandwiched around the  standard output
+ *
+ * Return: returns 1
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
