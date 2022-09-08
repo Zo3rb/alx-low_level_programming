04e63e14c25d0465fb8b2be3f7f4f9f15f1db826
Zo3rb <forsteve79@gmail.com>
2022-09-08 09:26:55 -0700

    Again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
 */
