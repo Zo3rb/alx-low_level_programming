cb369369f6c70c672769be9892f16b777f7db58f
Zo3rb <forsteve79@gmail.com>
2022-09-08 09:57:23 -0700

    Betty Styling

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
-#include<unistd.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: prints the standrad error sandwiched around the  standard output
+ * Description: prints the standrad error sandwiched around the  standard output.
  *
  * Return: returns 1
  */
@@ -11,3 +12,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
