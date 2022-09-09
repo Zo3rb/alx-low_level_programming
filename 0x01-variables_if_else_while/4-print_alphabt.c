060550830b5d3e47c79d91e6d5831b166548abc0
Zo3rb <forsteve79@gmail.com>
2022-09-09 06:58:04 -0700

    again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,18 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                if (ch == 'e' || ch == 'q')
-                        ch++;
-                putchar(ch);
-                ch++;
-        }
-        putchar('\n');
-        return (0);
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
 }
