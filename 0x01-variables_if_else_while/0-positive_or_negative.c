3eaa8d4092fc10d9f3f7c1ae43ce120468993c8c
Zo3rb <forsteve79@gmail.com>
2022-09-09 06:13:19 -0700

    betty is sux

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+
 /**
  * main - Entry point
  *
- * Description:  print whether the number stored
- * in the variable n is positive or negative.
+ * Description: print value of n satus; zero, positive or negative
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if (n < 0)
-        {
-                printf("%i is negative\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%i is zero\n", n);
-        }
-        else
-        {
-                printf("%i is positive\n", n);
-        }
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+
+	return (0);
 }
