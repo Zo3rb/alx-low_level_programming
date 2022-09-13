c985f5f1e0cda0016eb052963ce8ddd26261bd40
Zo3rb <forsteve79@gmail.com>
2022-09-13 08:28:30 -0700

    4

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: checking "4-alpha" code.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	
+	return (0);
+}
