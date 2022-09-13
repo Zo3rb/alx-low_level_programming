700cab8b032a607c7d00aab2eb43f058292bd395
Zo3rb <forsteve79@gmail.com>
2022-09-13 07:16:31 -0700

    0 again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "putchar.h"
 
 /**
  * main - Entry point
@@ -9,7 +9,15 @@
 */
 int main(void)
 {
-	printf("_putchar");
+	char c[9] = "putchar";
+	int i = 0;
+
+	while(i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
