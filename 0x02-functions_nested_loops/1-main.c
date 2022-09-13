551be1a308446de842f049c50fd4cc0345786e7f
Zo3rb <forsteve79@gmail.com>
2022-09-13 07:43:51 -0700

    alphabet again

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabets in lowercase.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
