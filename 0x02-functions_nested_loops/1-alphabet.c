891a931f946a47ca5a31fef53b0d73ce4e1f1830
Zo3rb <forsteve79@gmail.com>
2022-09-13 07:37:08 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase, 
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
