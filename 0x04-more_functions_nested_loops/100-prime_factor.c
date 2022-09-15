c9c754cf49b0060dc1c2f83de40e1efbeb1b43d5
Zo3rb <forsteve79@gmail.com>
2022-09-15 05:53:11 -0700

    Again

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <math.h>
+
 /**
  * main - prime numbers
  *
- * Return: Always 0.
+ * Return: 0
  */
+
 int main(void)
 {
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
 
-unsigned int long n = 612852475143, a = (int) sqrt(n);
-
-while (1)
-{
-
-	if (n % a == 0)
+	while (num != 1)
 	{
-		printf("%lu \n", n / a);
-		break;
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
 	}
-	a--;
-
-}
-
-return (0);
+	printf("%ld\n", larg_prim);
+	return (0);
 }
