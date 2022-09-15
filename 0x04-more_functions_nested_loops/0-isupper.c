de548bc605d1133d85714fe395831afabed564b7
Zo3rb <forsteve79@gmail.com>
2022-09-14 22:38:38 -0700

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - Entery point.
+ *
+ * Description: checks for uppercase character.
+ * @c: Character to check.
+ *
+ * Return: 1 or 0.
+ */
+int _isupper(char c)
+{
+	return (c >= 65 && c <= 90);
+}
