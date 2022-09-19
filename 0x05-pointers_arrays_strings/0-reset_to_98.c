3a882fadf972feea8dc3031a772fa8a1b42b7759
Zo3rb <forsteve79@gmail.com>
2022-09-19 02:55:20 -0700

    first attempt.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Description: overite a variable (int) value
+ * by a pointer of it.
+ * @n: the pointer of the int.
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
