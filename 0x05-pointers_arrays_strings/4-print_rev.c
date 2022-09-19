14c03d49c92f1c59638aa152f4fbf9014ca48651
Zo3rb <forsteve79@gmail.com>
2022-09-19 08:19:30 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - entery point.
+ *
+ * Description: prints a string, in reverse, followed by a new line.
+ * @s: string to reverse.
+ *
+ * Return: nothing.
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	while (i >= 0)
+	{
+		_outchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
