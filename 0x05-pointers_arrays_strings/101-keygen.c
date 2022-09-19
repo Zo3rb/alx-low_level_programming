e2ef9b1c9365dcca8e34d9e807546c3c694200b9
Zo3rb <forsteve79@gmail.com>
2022-09-19 10:09:51 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: generating random valid password.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+
+	return (0);
+}
