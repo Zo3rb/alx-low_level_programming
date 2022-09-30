ea4ff7e0476e52069d437fd7a84769e6b20dac3d
Zo3rb <forsteve79@gmail.com>
2022-09-30 13:13:24 -0700

    another solution

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
-gcc -c *.c
+gcc -c -Wall -Werror -Wextra *.c
 ar rc liball.a *.o
-rm *.o
