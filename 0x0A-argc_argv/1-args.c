37b90b1691ff04dca00b48d6fea6fde0ac603c60
Zo3rb <forsteve79@gmail.com>
2022-09-30 05:56:13 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a program prints arguments count these paased to.
+ * @argc: the count of the arguments passed to.
+ * @argv: the Arguments its self.
+ *
+ * Return: Always (0).
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n", argc);
+
+	return (0);
+}
