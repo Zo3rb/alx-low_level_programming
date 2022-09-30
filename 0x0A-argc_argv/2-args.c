1cd295a70dd65ab763600fbecfd1904de71f553a
Zo3rb <forsteve79@gmail.com>
2022-09-30 06:10:57 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a program that prints all args passed to.
+ * @argc: the count of the arguments passed to.
+ * @argv: the Arguments its self.
+ *
+ * Return: Always (0).
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
