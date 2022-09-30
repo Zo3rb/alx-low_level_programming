30433359173701c7af8336b69d62c75717786709
Zo3rb <forsteve79@gmail.com>
2022-09-30 04:57:59 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: a program that prints it's name.
+ * @argc: the count of the arguments passed to.
+ * @argv: the Arguments its self.
+ *
+ * Return: Always (0).
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", arv[0]);
+
+	return (0);
+}
