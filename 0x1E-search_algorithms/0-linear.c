8a67439364d1f7580ae133563ae2658ddce6716b
Zo3rb <ali.f90@outlook.com>
2023-04-13 03:59:21 -0700

    Betty-styling

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,13 +11,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-        if (array == NULL)
-                return (-1);
+	if (array == NULL) return (-1);
 
-        for (size_t i = 0; i < size; i++) {
-                if (array[i] == value)
-                        return (i);
+	for (size_t i = 0; i < size; i++)
+	{
+		
+		if (array[i] == value) return (i);
         }
 
-        return (-1);
+	return (-1);
 }
